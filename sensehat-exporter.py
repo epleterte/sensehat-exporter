565548e8e46da34f9c23ca38de8217ae6f0c930e
Christian Bryn <chr.bryn@gmail.com>
2020-01-05 03:54:55 +0100

    Add 'orientation' data, enabled by flag

--- sensehat-exporter.py
+++ sensehat-exporter.py
@@ -31,8 +31,9 @@ REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing requ
 
 class SenseHatCollector():
     """ This sets up a custom collector for the SenseHat """
-    def __init__(self):
+    def __init__(self, orientation=False):
         self.sense = SenseHat()
+        self.orientation = orientation
 
     @REQUEST_TIME.time()
     def collect(self):
@@ -47,6 +48,14 @@ class SenseHatCollector():
         metric.add_sample('rpi_sensehat_temperature', value=temperature, labels={'name': 'SenseHat Temperature'})
         metric.add_sample('rpi_sensehat_humidity', value=humidity, labels={'name': 'SenseHat Humidity'})
         metric.add_sample('rpi_sensehat_pressure', value=pressure, labels={'name': 'SenseHat Pressure'})
+        if self.orientation:
+            roll = self.sense.orientation['roll']
+            yaw = self.sense.orientation['yaw']
+            pitch = self.sense.orientation['pitch']
+            metric.add_sample('rpi_sensehat_roll', value=roll, labels={'name': 'SenseHat Roll'})
+            metric.add_sample('rpi_sensehat_yaw', value=yaw, labels={'name': 'SenseHat Yaw'})
+            metric.add_sample('rpi_sensehat_pitch', value=pitch, labels={'name': 'SenseHat Pitch'})
+
         yield metric
 
 
@@ -54,10 +63,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--port', nargs='?', const=9101, help='The TCP port to listen on', default=9101)
     parser.add_argument('--bind', nargs='?', const='0.0.0.0', help='The interface/IP to bind to', default='0.0.0.0')
+    parser.add_argument('--orientation', help='Output orientation data', action='store_true')
+    parser.set_defaults(orientation=False)
     args = parser.parse_args()
     log.info('listening on http://%s:%d/metrics', args.bind, int(args.port))
 
-    REGISTRY.register(SenseHatCollector())
+    REGISTRY.register(SenseHatCollector(orientation=args.orientation))
     start_http_server(int(args.port), addr=args.bind)
 
     while True:
